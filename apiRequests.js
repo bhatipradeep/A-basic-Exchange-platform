a332b8313791a064553d0978a5767c983bf91deb
Pradeepsingh Bhati <43387727+bhatipradeep@users.noreply.github.com>
2019-12-26 19:09:11 +0530

    Update apiRequests.js

--- apiRequests.js
+++ apiRequests.js
@@ -2,6 +2,6 @@ global.fetch = require('node-fetch');
 const cc = require('cryptocompare');
 
 //connecting to cryptocompare api;
-cc.setApiKey('d95a965ef463bdcfad9f50207b43e585537b4688d5db89433d773469db80dc3d');
+cc.setApiKey('');
 var api1 = cc;
 module.exports.cryptoCompareApi = api1;
